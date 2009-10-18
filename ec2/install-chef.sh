5c45b66023ad43a9cd8085a07d01c6201800ea90
Till Klampaeckel <till@php.net>
2009-10-18 17:12:15 +0200

    add source blog entry, silenced curl

--- ec2/install-chef.sh
+++ ec2/install-chef.sh
@@ -3,6 +3,9 @@
 #                                                                                  #
 # This script setups chef and all dependencies from opscode's APT repository.      #
 #                                                                                  #
+# This is from (+ rubygems1.8):                                                    #
+# http://blog.opscode.com/blog/2009/09/02/apt-repository-for-chef/                 #
+#                                                                                  #
 ####################################################################################
 #                                                                                  #
 # Author:  Till Klampaeckel                                                        #
@@ -16,7 +19,7 @@ RELEASE_NAME=jaunty
 
 echo "deb http://apt.opscode.com/ $RELEASE_NAME universe" >> /etc/apt/sources.list.d/opscode.list
 
-curl http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
+curl -s http://apt.opscode.com/packages@opscode.com.gpg.key | apt-key add -
 
 apt-get -y update
 apt-get -y install rubygems1.8
