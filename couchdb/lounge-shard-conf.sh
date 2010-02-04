50acb6e59a7c08af4645197b69d748cc146e10a8
Till Klampaeckel <till@php.net>
2010-02-04 16:56:36 +0100

    echo start command for each shard

--- couchdb/lounge-shard-conf.sh
+++ couchdb/lounge-shard-conf.sh
@@ -24,11 +24,19 @@ HTTPAUTHSECRET="this should be adjusted"
 # We'll start at this port
 PORT=5984
 
+CHROOT=/
+
 # this is the ebs volume we log to
-LOG_EBS=/logs
+LOG_EBS=${CHROOT}logs
 
 # this is the ebs the databases will be on
-DB_EBS=/couchdb_ebs
+DB_EBS=${CHROOT}couchdb_ebs
+
+# this is where couchdb is installed
+COUCHDB_EBS=${CHROOT}couchdb
+
+# this is the user to run couchdb with
+COUCHDB_USER=root
 
 ## Don't edit below.
 
@@ -41,6 +49,7 @@ function save_file {
 function create {
     mkdir $1;
     touch $2;
+    touch $3;
 }
 
 if [ -z $NUMSERVERS ]; then
@@ -63,9 +72,23 @@ do
     local_config=${local_config//LOGEBS/$LOG_EBS}
     local_config=${local_config//DBEBS/$DB_EBS}
 
+    pid_file="/var/run/couch-${shard_port}.pid"
+    db_dir="${DBS_EBS}/${shard_port}"
+    log_file="${LOG_EBS}/couch-${shard_port}.log"
+
     save_file "local-${shard_port}.ini" "$local_config"
 
-    #create "${DBS_EBS}/${shard_port}" "${LOG_EBS}/couch-${shard_port}.log"
+    #create "{$db_dir}" "${log_file}" "${pid_file}"
+
+    init_cmd="sudo su -c'/usr/bin/couchdb"
+    init_cmd="${init_cmd} -c ${COUCHDB_EBS}/etc/couchdb/local-${shard_port}.ini"
+    init_cmd="${init_cmd} -b -r 5"
+    init_cmd="${init_cmd} -p ${pid_file}"
+    init_cmd="${init_cmd} -o /dev/null -e /dev/null'"
+    init_cmd="${init_cmd} ${COUCHDB_USER}"
+
+    echo "${init_cmd}"
+
 done
 
 echo "Done creating ${NUMSERVERS} config files."
